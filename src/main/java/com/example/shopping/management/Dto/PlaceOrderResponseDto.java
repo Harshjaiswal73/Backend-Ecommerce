3a8d0ea5dfbf1e837c354ce21faf73cd73804c9a
backend_phantom <155292983+Harshjaiswal73@users.noreply.github.com>
2026-08-05 22:46:35 +0530

    Refactor PlaceOrderResponseDto by removing boilerplate
    Removed unnecessary constructor and getter/setter methods.

--- src/main/java/com/example/shopping/management/Dto/PlaceOrderResponseDto.java
+++ src/main/java/com/example/shopping/management/Dto/PlaceOrderResponseDto.java
@@ -16,37 +16,4 @@ public class PlaceOrderResponseDto {
 	private String message;
 	
 	private OrderStatus orderStatus;
-
-	
-	
-	public PlaceOrderResponseDto() {
-		super();
-		this.orderId = orderId;
-		this.message = message;
-		this.orderStatus = orderStatus;
-	}
-
-	public Long getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public OrderStatus getOrderStatus() {
-		return orderStatus;
-	}
-
-	public void setOrderStatus(OrderStatus orderStatus) {
-		this.orderStatus = orderStatus;
-	}
 }
